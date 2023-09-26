9cf202e67786a1e8f02ccd7c05eaf6121dcfdf44
Gus-Wolf <agw5265@psu.edu>
2023-09-26 12:57:54 -0400

    updated card

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,50 +1,70 @@
-//toggle button
-const details = document.querySelector('summary');
-
-document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
-  if (details.parentNode.getAttribute('open')) {
-        details.parentNode.removeAttribute('open');
-  }
-  else {
-    details.parentNode.setAttribute('open','open');    
-  }
-});
-
-//duplicate button
-document.querySelector('.duplicate').addEventListener('click', function(e) {
-  const cardToClone = document.querySelector('.card').cloneNode(true);
-  document.body.appendChild(cardToClone);
-});
-
-//delete button
-document.querySelector('#deletecard').addEventListener('click', function(e) {
-  let wantsTo = confirm("Are you sure?");
-  if (wantsTo) {
-    if (document.querySelector('.card:last-child') !== document.querySelector('.card')) {
-      document.querySelector('.card:last-child').remove();      
+// 
+function setupCardListeners(card){
+  card.addEventListener("click", function(e){
+    this.classList.toggle("highlighted");
+  });
+  
+  // This block is used to change the card color using an if statement.
+  const colorToggleBtn = card.querySelector('.color-toggle-button');
+  colorToggleBtn.addEventListener('click', function(){
+    const cardContent = document.querySelector('.card-content');
+    if(cardContent.classList.contains('hotpink-bg')){
+      cardContent.classList.remove('hotpink-bg');
+    }else{
+      cardContent.classList.add('hotpink-bg');
     }
-    else {
-      alert("Cannot original last card");
+  });
+  
+  // This block is used to change the title to something else
+  const changeTitleBtn = card.querySelector('.change-title-button');
+  changeTitleBtn.addEventListener('click', function(e){
+    e.stopPropagation();
+    const title = this.closest('.card-content').querySelector('.card-title');
+    if (title.textContent === "Lightning McQueen"){
+      title.textContent = "something Else";
+    }else{
+      title.textContent = "Lightning McQueen";
     }
-  }
-});
+  });
+  
+  // Creates the details button that hides or shows the details of the card 
+  const detailsLink = card.querySelector('.details-button');
+  detailsLink.addEventListener('click', function(e){
+    e.preventDefault();
+    const description = this.closest('.card-content').querySelector('.card-description');
+    description.classList.toggle('hidden');
+  });
+}
 
-//change title button
-document.querySelector('.changetxt').addEventListener('click', function(e) {
-  let name = "Something else";
-  if (name) {
-    document.querySelector('.card h3').innerText = name;
-  }
+// This block of text clones the entirety of the card
+document.getElementById('btn').addEventListener('click', function(){
+     const wrapper = document.getElementById('wrapper');
+     const cardClone = wrapper.querySelector('.card').cloneNode(true);
+wrapper.appendChild(cardClone);
+setupCardListeners(cardClone);
+handleCardHover(cardClone);
 });
+const deleteButton = document.getElementById('deleteBtn');
 
-//change background
-const card = document.querySelector('#card');
-        let backgroundColor = false;
-    document.querySelector('#changebackground').addEventListener('click', function(e) {
-            if (backgroundColor) {
-                card.style.backgroundColor = 'white';
-            } else {
-                card.style.backgroundColor = 'gray';
-            }
-            backgroundColor = !backgroundColor;
-        });
+deleteButton.addEventListener('click', function(){
+const cards = document.querySelectorAll('.card');
+const lastCard = cards[cards.length - 1];
+if (cards.length > 1){
+  lastCard.remove();
+}
+  });
+
+// This block is designed to change the background color when you hover over the card
+function handleCardHover(card){
+  card.addEventListener('mouseenter', function(){
+                        this.classList.add('highlighted');
+                        });
+
+card.addEventListener('mouseleave', function (){
+  this.classList.remove('highlighted');
+});
+}
+
+const intitalCard = document.querySelector('.card');
+setupCardListeners(intitalCard);
+handleCardHover(intitalCard);
